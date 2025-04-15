b0fb2b0de95c717d0da0dc444175f0f4a6c8adb2
Alec Armbruster <35377827+alectrocute@users.noreply.github.com>
2025-04-15 13:48:40 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@ echo "Starting setup script!";
 sudo apt-get update -y;
 sudo apt-get install git unclutter;
 cd /home;
-git clone https://github.com/alectrocute/pi-kiosk;
+sudo git clone https://github.com/alectrocute/pi-kiosk;
 cd pi-kiosk;
-mkdir -p /home/pi/kiosk;
-cp kiosk.sh /home/pi/kiosk/kiosk.sh
+sudo mkdir -p /home/pi/kiosk;
+sudo cp kiosk.sh /home/pi/kiosk/kiosk.sh
 sudo cp kiosk.service /lib/systemd/system/kiosk.service;
 sudo systemctl enable kiosk.service;
 echo "Setup complete!";
