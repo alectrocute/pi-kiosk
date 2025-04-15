150a412be4cda7cca9b43a3a3ee1e40d4f970e0b
Alec Armbruster <35377827+alectrocute@users.noreply.github.com>
2025-04-15 13:55:12 -0700

    wip

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ sudo rm -rf ./pi-kiosk /home/pi/kiosk;
 sudo git clone https://github.com/alectrocute/pi-kiosk;
 cd pi-kiosk;
 sudo mkdir -p /home/pi/kiosk;
-sudo cp kiosk.sh /home/pi/kiosk/kiosk.sh
+sudo cp kiosk.sh /home/pi/kiosk/kiosk.sh;
 sudo cp kiosk.service /lib/systemd/system/kiosk.service;
 sudo systemctl enable kiosk.service;
 echo "Setup complete!";
