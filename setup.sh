ed2cd7d6233964bcc48a2a9e954fedd84f7201d7
Alec Armbruster <35377827+alectrocute@users.noreply.github.com>
2025-04-15 13:57:14 -0700

    wip

--- setup.sh
+++ setup.sh
@@ -1,14 +1,28 @@
 echo "Starting setup script!";
+
+# Remove any existing kiosk installation.
+sudo rm -rf /home/pi-kiosk;
+sudo rm -rf /home/pi;
+sudo systemctl stop kiosk;
+sudo systemctl disable kiosk;
+sudo rm -rf /lib/systemd/system/kiosk.service;
+
+# Install dependencies.
 sudo apt-get update -y;
 sudo apt-get install git unclutter -y;
+
+# Clone the repository.
 cd /home;
-sudo rm -rf ./pi-kiosk /home/pi/kiosk;
 sudo git clone https://github.com/alectrocute/pi-kiosk;
 cd pi-kiosk;
+
+# Install the kiosk.
 sudo mkdir -p /home/pi/kiosk;
 sudo cp kiosk.sh /home/pi/kiosk/kiosk.sh;
 sudo cp kiosk.service /lib/systemd/system/kiosk.service;
 sudo systemctl enable kiosk.service;
 echo "Setup complete!";
+
+# Start the kiosk.
 sleep 6;
 sudo systemctl start kiosk;
