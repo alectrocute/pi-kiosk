95c9582a6fa2a5e2bf514a61e39be7e8b7803c7f
Alec Armbruster <35377827+alectrocute@users.noreply.github.com>
2025-04-15 21:16:43 -0700

    wip

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-generate_success_message() {
+log_msg() {
     COLOR_GREEN='\e[0;32m'
     COLOR_RESET='\e[0m'
     echo -e "rocket-kiosk: ${COLOR_GREEN}${1}${COLOR_RESET}";
@@ -12,34 +12,34 @@ GIT_REPO_PATH="$LOCAL_USER_HOME_PATH/pi-kiosk";
 EXEC_PATH="$LOCAL_USER_HOME_PATH/kiosk";
 SERVICE_PATH="/lib/systemd/system/kiosk.service";
 
-generate_success_message "Removing any existing kiosk installation...";
+log_msg "Removing any existing kiosk installation...";
 sudo mkdir -p $LOCAL_USER_HOME_PATH;
 cd $LOCAL_USER_HOME_PATH;
 sudo rm -rf $GIT_REPO_PATH $EXEC_PATH;
-generate_success_message "Stopping kiosk service...";
+log_msg "Stopping kiosk service...";
 sudo systemctl stop kiosk;
 sudo systemctl disable kiosk;
-generate_success_message "Removing kiosk service...";
+log_msg "Removing kiosk service...";
 sudo rm -rf $SERVICE_PATH;
 
-generate_success_message "Updating package list...";
+log_msg "Updating package list...";
 sudo apt-get update -y;
-generate_success_message "Installing dependencies...";
+log_msg "Installing dependencies...";
 sudo apt-get install git -y;
 
-generate_success_message "Cloning the repository...";
+log_msg "Cloning the repository...";
 sudo git clone $GIT_REPO_URL;
 cd pi-kiosk;
 
-generate_success_message "Installing the kiosk...";
+log_msg "Installing the kiosk...";
 sudo mkdir -p $EXEC_PATH;
 sudo cp kiosk.sh $EXEC_PATH/kiosk.sh;
 sudo chmod +x $EXEC_PATH/kiosk.sh;
 sudo cp kiosk.service $SERVICE_PATH;
-generate_success_message "Enabling kiosk service...";
+log_msg "Enabling kiosk service...";
 sudo systemctl enable kiosk.service;
-generate_success_message "Setup complete!";
+log_msg "Setup complete!";
 
 # Start the kiosk.
-generate_success_message "Starting the kiosk...";
+log_msg "Starting the kiosk...";
 sudo systemctl start kiosk;
