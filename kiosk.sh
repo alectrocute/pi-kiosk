3279616a5723b82bd4ad1c70f1a57f532f89eb1c
Alec Armbruster <35377827+alectrocute@users.noreply.github.com>
2025-04-15 21:21:43 -0700

    wip

--- kiosk.sh
+++ kiosk.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Wait for services to come online.
-# TODO: It would be nice to get rid of this, but right now on Bookworm, if we
-# don't wait, there are errors at boot and you have to start kiosk manually.
 sleep 8
 
 flags=(
